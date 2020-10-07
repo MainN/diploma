4127a9579c53d960216a69e574bb43889856f372
MainN <VishiiTamplier@yandex.ru>
2020-10-07 21:32:15 +0300

    adding tutorials

--- pymc4/model.py
+++ pymc4/model.py
@@ -3,19 +3,47 @@ import pymc4 as pm
 import numpy as np
 import arviz as az
 
+import timeit
 import tensorflow as tf
 import tensorflow_probability as tfp
-import matplotlib.pyplot as plt
-@pm.model
-def model(x):
-    # prior for the mean of a normal distribution
-    loc = yield pm.Normal('loc', loc=0, scale=10)
-    
-    # likelihood of observed data
-    obs = yield pm.Normal('obs', loc=loc, scale=1, observed=x)
-# 30 data points normally distributed around 3
-x = np.random.randn(30) + 3
 
-# Inference
-trace = pm.sample(model(x))
-plt.show(az.plot_posterior(trace, var_names=['model/loc']))
+import seaborn as sns
+import matplotlib.pyplot as plt
+tfd = tfp.distributions
+N = 6
+std = 1
+m = np.random.normal(0, scale=5, size=1).astype(np.float32)
+b = np.random.normal(0, scale=5, size=1).astype(np.float32)
+x = np.linspace(0, 100, N).astype(np.float32)
+y = m*x+b+ np.random.normal(loc=0, scale=std, size=N).astype(np.float32)
+def joint_log_prob(x, y, m, b, std):
+  rv_m = tfd.Normal(loc=0, scale=5)
+  rv_b = tfd.Normal(loc=0, scale=5)
+  rv_std = tfd.HalfCauchy(loc=0., scale=2.)
+
+  y_mu = m*x+b
+  rv_y = tfd.Normal(loc=y_mu, scale=std)
+
+  return (rv_m.log_prob(m) + rv_b.log_prob(b) + rv_std.log_prob(std)
+          + tf.reduce_sum(rv_y.log_prob(y)))
+def target_log_prob_fn(m, b, std):
+    return joint_log_prob(x, y, m, b, std)
+
+hmc_kernel = tfp.mcmc.HamiltonianMonteCarlo(
+  target_log_prob_fn=target_log_prob_fn,
+  step_size=np.float64(0.000000000000008),
+  num_leapfrog_steps=7)
+
+states,kernel_results=tfp.mcmc.sample_chain(num_results=1000,
+current_state=[
+          0.01 * tf.ones([], name='init_m', dtype=tf.float32),
+          0.01 * tf.ones([], name='init_b', dtype=tf.float32),
+          1. * tf.ones([], name='init_std', dtype=tf.float32)
+      ],kernel=hmc_kernel)
+colors = ['b', 'g', 'r']
+print("Acceptance rate:", kernel_results.is_accepted.numpy().mean())
+for i in range(3):
+  sns.distplot(states[i], color=colors[i])
+ymax = plt.ylim()[1]
+plt.ylim(0, ymax)
+plt.show()
---
+++ resourses/list.md
@@ -0,0 +1,3 @@
+# Tutorials
+https://github.com/tensorflow/probability/blob/master/tensorflow_probability/examples/jupyter_notebooks/Eight_Schools.ipynb
+https://github.com/tensorflow/probability/blob/master/tensorflow_probability/examples/logistic_regression.py
