bf67c3d0ccc181da815a12a23d18f8883ddc7635
MainN <VishiiTamplier@yandex.ru>
2020-10-16 23:28:54 +0300

    trying modle

--- pyro/linear.py
+++ pyro/linear.py
@@ -11,3 +11,10 @@ import pyro.poutine as poutine
 import pyro.distributions as dist
 from pyro.infer.mcmc.api import MCMC
 from pyro.infer.mcmc.hmc import HMC
+def linear(xes,yes):
+    slope = pyro.sample("slope",dist.Normal(5,10))
+    intercept = pyro.sample("intercept",dist.Normal(0,10))
+    var = pyro.sample("var",dist.InverseGamma(3, 0.1))
+    x=slope*xes
+    return slope
+print(linear([1],[1]))
